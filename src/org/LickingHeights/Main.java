823a1dcac742854fb2277195bb9704e99e259fe2
Anish <Anish.lohar.2022@student.lhschools.org>
2019-09-26 09:53:35 -0400

    Added todo stuff

--- src/org/LickingHeights/Main.java
+++ src/org/LickingHeights/Main.java
@@ -4,5 +4,37 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+	    // todo declare variable a,b,c,discriminant, axisOfSymmetry,parabolFacing,x,y
+
+        //todo initialize manually
+
+        //todo do calculation
+
+        //todo print results
+
+        //todo not code check results
+
+        //todo initiialize scanner and redo the initialize
+
+        //todo double check results
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
